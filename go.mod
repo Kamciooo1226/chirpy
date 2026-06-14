08ef07b6edb6347cb8894a1eeb5ad38c9157add8
Kamciooo1226 <kbogusz1226@gmail.com>
2026-06-14 23:55:27 +0200

    Create a Server and Handlers for files and Health endpoint

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Kamciooo1226/chirpy
+
+go 1.26.2
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
